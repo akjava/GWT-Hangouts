a9fa28b1b11aefb4cf028c6ccbe1ad471ad418ac
aki <aki@xps>
2013-02-16 21:43:05 +0900

    upgrade layout to v1.3

--- src/com/akjava/gwt/lib/hangouts/client/layout/VideoFeed.java
+++ src/com/akjava/gwt/lib/hangouts/client/layout/VideoFeed.java
@@ -1,8 +1,13 @@
 package com.akjava.gwt.lib.hangouts.client.layout;
 
+import com.akjava.gwt.lib.hangouts.client.StringMap;
 import com.google.gwt.core.client.JavaScriptObject;
 
 public class VideoFeed extends JavaScriptObject {
 protected VideoFeed(){}
 
+
+public  final native String getDisplayedParticipant() /*-{
+return this.getDisplayedParticipant()
+	}-*/;
 }
